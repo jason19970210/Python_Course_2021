fa56ccaeb9dd2b568c382d2d9f6a6682fd587f4a
jason19970210 <jason19970210@gmail.com>
2021-03-04 03:55:42 -0800

    upload w2 exercise

---
+++ exercise_w2.py
@@ -0,0 +1,17 @@
+try:
+    year = int(input("年份: "))
+    if year < 1800:
+        print("Please input year above 1800 !")
+    else: # input > 1800
+        if (year % 4) == 0:
+            if (year % 100) == 0:
+                if (year % 400) == 0:
+                    print(f"{year}年 是閏年")  # % 400 == 0
+                else:
+                    print(f"{year}年 不是閏年") # % 100 == 0
+            else:
+                print(f"{year}年 是閏年")       #  %4 == 0
+        else:
+            print(f"{year}年 不是閏年")
+except:
+    print("Not a vaild year !")
